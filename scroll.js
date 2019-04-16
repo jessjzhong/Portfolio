708d2579b70690a9f11564f678c04206cb131e85
Jessica Zhong <jesssicazhong@gmail.com>
2019-04-15 23:00:25 -0700

    added scroll

--- scroll.js
+++ scroll.js
@@ -1,24 +1,10 @@
-var homePage = {
-    smoothScroll: function () {
-        $('a[href*="#"]:not([href="#"])').click(function () {
-            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-                var target = $(this.hash);
 
-                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+$("a[href^='#']").click(function (e) {
+    e.preventDefault();
 
-                if (target.length) {
-                    $('html, body').animate({
-                        scrollTop: target.offset().top - 100
-                    }, 750, 'easeInOutExpo');
-                    return false;
-                }
-            }
-        });
-    },
+    var position = $($(this).attr("href")).offset().top;
 
-    ready: function () {
-        homePage.smoothScroll();
-    }
-};
-
-$(document).ready(homePage.ready);
+    $("body, html").animate({
+        scrollTop: position
+    } ,800);
+});
