3e5d6c02996af09614ebf25c6c6f3acb28d6b76c
Jessica Zhong <jesssicazhong@gmail.com>
2019-04-18 14:27:38 -0700

    removed JS

--- scroll.js
+++ scroll.js
@@ -1,26 +1,14 @@
 
-$("a[href^='#']").click(function (e) {
-    e.preventDefault();
+// $("a[href^='#']").click(function (e) {
+//     e.preventDefault();
 
-    var position = $($(this).attr("href")).offset().top;
+//     var position = $($(this).attr("href")).offset().top;
 
-    $("body, html").animate({
-        scrollTop: position
-    } ,2000);
-});
+//     $("body, html").animate({
+//         scrollTop: position
+//     } ,2000);
+// });
 
 
-var btn = $('#backbtn');
 
-$(window).scroll(function () {
-    if ($(window).scrollTop() > 300) {
-        btn.addClass('show');
-    } else {
-        btn.removeClass('show');
-    }
-});
 
-btn.on('click', function (e) {
-    e.preventDefault();
-    $('html, body').animate({ scrollTop: 0 }, '300');
-});
