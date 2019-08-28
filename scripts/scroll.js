651f57953ee228121573f56f6882d3bfca94cbe9
Jessica Zhong <jesssicazhong@gmail.com>
2019-08-28 16:31:22 -0700

    new bottom links

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -26,3 +26,20 @@ $(document).ready(function () {
 // close hamburger menu after click
 Open: $('.menu-wrap input').prop("checked", true);
 Close: $('.menu-wrap input').prop("checked", false);
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
